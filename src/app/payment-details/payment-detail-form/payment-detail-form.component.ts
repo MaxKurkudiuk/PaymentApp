3acde452ac7a04b27a82f182fbea1ce8c20d59ab
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2026-05-30 10:13:52 +0300

    feat: enhance payment detail form with reactive forms and toastr notifications

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,27 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PaymentDetailService } from '../../shared/payment-detail.service';
 import { PaymentDetail } from '../../shared/payment-detail.model';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-detail-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './payment-detail-form.component.html',
   styles: ``,
 })
 export class PaymentDetailFormComponent {
-  constructor(public service: PaymentDetailService) {
+  constructor(public service: PaymentDetailService, private toastr: ToastrService) {
   }
 
-  onSubmit(event: Event) {
+  onSubmit(form: NgForm) {
+  // onSubmit(event: Event) {
     // event.preventDefault();
 
     this.service.postPaymentDetail()
     .subscribe({
       next: res => {
         this.service.list = res as PaymentDetail[];
-        this.service.resetForm();
+        this.service.resetForm(form);
+        this.toastr.success('Payment detail added successfully!', 'Payment Detail Register');
       },
-      error: err => {console.log(err)}
+      error: err => {
+        this.toastr.error('Failed to add payment detail.', 'Error');
+      }
     });
   }
 }
